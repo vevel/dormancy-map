6c17ff31cf13494e5309eb48dae64614e3c287e5
Uemit Seren <uemit.seren@gmail.com>
2015-09-25 14:11:50 +0200

    Initial checkin

---
+++ README.md
@@ -0,0 +1,18 @@
+The Dormancy Map
+==
+The [Dormancy Map](http://gregor-mendel-institute.github.io/dormancy-map/) is an interactive web-based tool, that allows to interactively explore the fine-scale picture of dormancy for the 1135 _A. thaliana_ accession.
+
+
+![Admixture-map](https://raw.githubusercontent.com/Gregor-Mendel-Institute/dormancy-map/master/preview.png "Dormancy Map")
+
+For each accession a piechart is displayed. The slices correspond to the various fractions of the ADMIXTURE cluster.
+Users can change the number of clusters K by clicking on the gear icon in the top left corner (by default data for K=9 is shown).
+
+
+Because of the number of accessions (1135) nearby accessions are clustered together and an averaged piechart is displayed. The number inside the piechart shows the number of accessions that are clustered together. Clicking on a cluster will zoom into the region and display piecharts for individual accessions or smaller clusters.
+
+The list on the left side shows all accessions that are visible in the current view. Zooming into specific regions either by clicking on a cluster or using the Google Maps zoom control will update the list.
+Hovering the mouse over a cluster will filter the list to display only the accessions that are part of the corresponding cluster and in addition to show a popup with a more detailed version of the piechart.
+
+Hovering the mouse over an accession in the list will display a pin on the map at its location and clicking on the list item will zoom into that position.
+A searchbox above the list allows the user to search for a specific accession using either the ID or the name.
---
+++ app/scripts/app.js
@@ -0,0 +1,117 @@
+/* global google */
+(function (document) {
+  'use strict';
+
+  var app = document.querySelector('#app');
+  app.showSpinner = true;
+  app.loaddata = true;
+  app.mapLoaded = false;
+
+  app.apiKey = 'AIzaSyB1GbyVSIOK12RJbFMkaIJjwhVNG-b8fjc';
+  app.dataSources = {
+    haplotype: {
+      id:'1TfD99thQGTo9XxMSWwx1QsNQBb2H_FhcPJJuryjc',
+      columns:['class_1','class_2','class_3','class_4','class_5','class_6','class_7','class_8'],
+      colors:['#36648b','#5cacee','#cd6600','#ff7f00','#ee9a49','#d2b48c','#999999','#cccccc'],
+      labels:['WD1','WD2','SD1','SD2','SD3','SD4','UD1','UD2'],
+      label: 'DOG1 Haplotype'
+      },
+    dormancy: {
+      id:'1rKBG1ciSaQJA-wlfzaeBvQpyXqVcLWXE86x7bYSe',
+      columns:['class_1','class_2','class_3','class_4','class_5'],
+      colors:['#EE7F00','#FFA54F','#CCCCCC','#63B8FF','#4F94CD'],
+      labels:['0 - 20','20 - 40','40 - 60','60 - 80',' 80 - 100'],
+      label: 'Germination Rate (GR21)'
+    }
+  };
+
+  app.displayInstalledToast = function () {
+    // Check to make sure caching is actually enabled—it won't be in the dev environment.
+    if (!document.querySelector('platinum-sw-cache').disabled) {
+      document.querySelector('#caching-complete').show();
+    }
+  };
+  app._getColors = function(route) {
+    return this._getCurrentDataSource(route).colors;
+  };
+
+  app._getCurrentDataSource = function(route) {
+    return this.dataSources[route];
+  };
+
+  app._getMarkers = function(response) {
+    var markers = [];
+    if (response === null) {
+      return markers;
+    }
+    if (this.loaddata) {
+      this.$.localstorage.value = response;
+    }
+    this.loaddata = false;
+    var numRows = response.rows.length;
+    for (var i = 0; i < numRows; i++) {
+      var row = response.rows[i];
+      var pieValues = [];
+      var accId = row[0];
+      var name = row[1];
+      var lng = row[2];
+      var lat = row[3];
+      var coordinate = new google.maps.LatLng(lat, lng);
+      var gr21 = row[5];
+      for (var j = 6; j < row.length; j++) {
+        pieValues.push(row[j] * 100);
+      }
+      var marker = document.createElement('admixture-marker');
+      marker.position = coordinate;
+      marker.data = pieValues;
+      marker.accId = accId;
+      marker.colors = this._getColors(this.route);
+      marker.name = name;
+      marker.gr21 = gr21;
+      markers.push(marker);
+    }
+    this.showSpinner = false;
+    return markers;
+  };
+
+
+  // Listen for template bound event to know when bindings
+  // have resolved and content has been stamped to the page
+  app.addEventListener('dom-change', function () {
+
+  });
+
+  // See https://github.com/Polymer/polymer/issues/1381
+  window.addEventListener('WebComponentsReady', function () {
+
+  });
+
+  app._getFusionParams = function(route) {
+    var datasource = this.dataSources[route];
+    var query = 'SELECT tg_ecotypeid,name,longitude,latitude,class_col,GR21,' + datasource.columns.join(',') + ' FROM ' + datasource.id;
+    return { sql: query, key: this.apiKey };
+  };
+  app.openInfoWindow = function() {
+    app.$.infoDialog.open();
+  };
+  app.googleMapLoaded = function() {
+     if (this.loaddata) {
+       this.$.datasource.generateRequest();
+     } else {
+       this.fusionresponse = this.$.localstorage.value;
+     }
+     this.mapLoaded = true;
+  };
+  app.loadDataFromBackend = function() {
+    this.loaddata = true;
+    if (this.mapLoaded) {
+       this.$.datasource.generateRequest();
+    }
+  };
+  app.onLoadDataFromCache = function() {
+    this.loaddata = false;
+    if (this.mapLoaded) {
+      this.fusionresponse = this.$.localstorage.value;
+    }
+  };
+})(document);
